1a00dde56d334c62d32beafff985e654cb767af4
hyerinnn <eun8229@gmail.com>
2023-06-13 20:39:27 +0900

    회원 서비스, 구현체 생성

---
+++ src/main/java/hello/core/member/Grade.java
@@ -0,0 +1,9 @@
+package hello.core.member;
+
+
+// 회원 등급
+// enum =  상수의 집합 클래스 (열거형 클래스)
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 부모 MemberRepository는 선언만 하고, 여기서 메소드내용을 구현한다.(implements, 오버라이딩)
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
