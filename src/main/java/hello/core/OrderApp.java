62d0b9749d391ba8d379655dd091fdc388d5f61b
hyerinnn <eun8229@gmail.com>
2023-06-19 22:24:37 +0900

    빈출력테스트

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class OrderApp {
     public static void main(String[] args) {
-/*        MemberService memberService = new MemberServiceImpl();
+/*      MemberService memberService = new MemberServiceImpl();
         OrderService orderService = new OrderServiceImpl();
-        */
+*/
 
 /*
 
